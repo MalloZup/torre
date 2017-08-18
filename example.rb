0ee9449eae532a6982fa720eba52606fa11a2aa9
Dario Maiocchi <dmaiocchi@suse.com>
2017-08-18 18:11:34 +0200

    adding more lib

--- example.rb
+++ example.rb
@@ -8,9 +8,11 @@ require_relative 'lib/torre_backend.rb'
 # MAIN example
 #
 
-t = 5
+t = 300
 gitlab = 'https://about.gitlab.com/'
 test0 = Torre.new(gitlab, t)
 test0.gather_responses
 puts test0.res_times
+puts 'db results'
+# good if you want to do something persistent with db
 test0.res_times_todb
