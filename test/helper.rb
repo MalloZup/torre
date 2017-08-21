7d39368db405bc40815575b980399dbc62e4e801
Dario Maiocchi <dmaiocchi@suse.com>
2017-08-21 09:00:17 +0200

    fix import

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,4 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
-require 'minitest/mock'
 project_root = File.dirname(File.absolute_path(__FILE__))
-Dir.glob(project_root + '/../lib/*') { |file| require file }
+Dir.glob(project_root + '/../lib/*.rb') { |f| require f }
