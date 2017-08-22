fca4caeba765cb369bf9e409eea4fa908f94697e
Dario Maiocchi <dmaiocchi@suse.com>
2017-08-22 13:30:37 +0200

    Adding sinatra and chartjs support

--- example.rb
+++ example.rb
@@ -1,18 +1,30 @@
 #! /usr/bin/ruby
 
-require 'net/http'
-require 'timeout'
-require 'uri'
+require 'sinatra'
+require 'haml'
+require 'time'
+require 'json'
+require 'fileutils'
 require_relative 'lib/torre_backend.rb'
+require_relative 'lib/torre_database.rb'
 
-# MAIN example
-#
-
-t = 3
+# get over 5 min results
+t = 300
 gitlab = 'https://about.gitlab.com/'
+puts "gathering https reponses over #{t}seconds please wait"
 test0 = Torre.new(gitlab, t)
 test0.gather_responses
 puts test0.res_times
 puts 'db results'
 # good if you want to do something persistent with db
 test0.res_times_todb
+
+# web chart result on http://localhost:4567/
+get '/' do
+  @res_times = []
+  hres = HttpRes.all
+  hres.each do |elem|
+    @res_times.push(elem.real_time)
+  end
+  haml :index
+end  
--- lib/torre_web.rb
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/ruby
-
-require 'sinatra'
-require 'haml'
-
-get '/' do
-  @test0 = 'Welcome to torre'
-  haml :index
-end
